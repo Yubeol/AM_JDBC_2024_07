3d4f81d61d791df2649bcb960827daaa63f6dfe9
Yubeol <mingu30638@gmail.com>
2024-07-09 14:18:18 +0900

    AM_JDBC_2024_07

--- src/main/java/org/koreait/Main.java
+++ src/main/java/org/koreait/Main.java
@@ -1,12 +1,10 @@
 package org.koreait;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+
 
 public class Main {
     public static void main(String[] args) {
